05beae52eb05bda05da2c6c5e50f0b3129ecbf0a
Louis THEVENET <louis.tvnt@gmail.com>
2023-08-04 22:02:18 +0200

    removed some comments + added GC

--- flake.nix
+++ flake.nix
@@ -9,41 +9,43 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{  
-  self, 
-  nixpkgs,
-  home-manager,  
-  ...  }:
- {
-    nixosConfigurations = {
-      louis = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.louis = { pkgs, ... }: {
-              home.username = "louis";
-              home.homeDirectory = "/home/louis";
-              programs.home-manager.enable = true;
-              home.packages = with pkgs; [
-                hello
-                blueman
-                spotify
-                kitty
-                discord
-                vim
-                firefox
-                flameshot
-                git
-              ];
-              home.stateVersion = "23.05";
-            };
-          }
-        ];
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , home-manager
+    , ...
+    }:
+    {
+
+      nixosConfigurations = {
+        louis = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.louis = { pkgs, ... }: {
+                home.username = "louis";
+                home.homeDirectory = "/home/louis";
+                programs.home-manager.enable = true;
+                home.packages = with pkgs; [
+                  hello
+                  blueman
+                  spotify
+                  kitty
+                  discord
+                  vim
+                  firefox
+                  flameshot
+                  git
+                ];
+                home.stateVersion = "23.05";
+              };
+            }
+          ];
+        };
       };
     };
-  };
 }
