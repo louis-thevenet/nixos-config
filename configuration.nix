35406591f7d5f937bcd07f52ad9df6f1b0485a23
Louis THEVENET <louis.tvnt@gmail.com>
2023-08-04 21:19:03 +0200

    added nix syntax support in codium

--- configuration.nix
+++ configuration.nix
@@ -172,7 +172,7 @@ home-manager.users.louis.programs = {
 	ms-dotnettools.csharp
 	rust-lang.rust-analyzer
         ms-vscode.cpptools
-        #jnoortheen.nix-ide
+        jnoortheen.nix-ide
       ]; #++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [ 
         #{
         #  name = "nunjucks-template";
