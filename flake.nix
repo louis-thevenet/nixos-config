d463b96b228a7df9e7229e3fed47fe4a4ed2ac9d
Louis THEVENET <louis.tvnt@gmail.com>
2023-08-04 21:11:39 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "flake for louis with Home Manager enabled";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # home manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{  
+  self, 
+  nixpkgs,
+  home-manager,  
+  ...  }:
+ {
+    nixosConfigurations = {
+      louis = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.louis = { pkgs, ... }: {
+              home.username = "louis";
+              home.homeDirectory = "/home/louis";
+              programs.home-manager.enable = true;
+              home.packages = with pkgs; [
+                hello
+                blueman
+                spotify
+                kitty
+                discord
+                vim
+                firefox
+                flameshot
+                git
+              ];
+              home.stateVersion = "23.05";
+            };
+          }
+        ];
+      };
+    };
+  };
+}
