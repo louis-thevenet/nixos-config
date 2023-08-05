effc16f58babd3a1aed7781bbfe8935ad9fc8af9
Louis THEVENET <louis.tvnt@gmail.com>
2023-08-05 19:31:55 +0200

    Added touchpad support for laptop

--- nixos/common.nix
+++ nixos/common.nix
@@ -79,7 +79,7 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
 
   system.stateVersion = "23.05";
 
