976313f73be68429121890691b00b958a68e2ed4
Louis THEVENET <louis.tvnt@gmail.Com>
2023-08-13 20:45:48 +0200

    switched to unstable

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
