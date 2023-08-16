93ad162823005680e58d310f8a6f17d5ff860c32
Louis THEVENET <louis.tvnt@gmail.Com>
2023-08-16 13:01:08 +0200

    changed exa alias to something easier to type

--- home-manager/oh-my-zsh.nix
+++ home-manager/oh-my-zsh.nix
@@ -6,7 +6,7 @@
       update-laptop = "sudo nixos-rebuild --flake .#louislaptop switch";
       home-update = "home-manager --flake .#louis@nixos switch";
 
-      exa-l = "exa -l --header -b";
+      lx = "exa -l --header -b";
 
 
     };
