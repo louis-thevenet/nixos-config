4380bf409a66656ff1b68e703319d44e092a61da
Louis THEVENET <louis.tvnt@gmail.Com>
2023-09-09 11:05:01 +0200

    removed unnecessary systems

--- env/flake.nix
+++ env/flake.nix
@@ -1,22 +1,25 @@
 {
-  description = "A Nix-flake-based Nix development environment";
+  description = "A flake-based Nix development environment";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            home-manager
-          ];
-        };
-      });
-    };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          home-manager
+        ];
+      };
+    });
+  };
 }
