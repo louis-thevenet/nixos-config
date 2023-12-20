e36628399a9475f5440e45d84c9571d8f1348287
Louis THEVENET <louis.tvnt@gmail.com>
2023-12-20 13:15:18 +0100

    Switched to unstable-small channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
