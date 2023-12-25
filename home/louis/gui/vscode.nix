c73e9dcb696c0ee9ad9d2017c5b349cc650de71d
Louis THEVENET <louis.tvnt@gmail.com>
2023-12-25 15:46:18 +0100

    Vscode : -> insider build

--- home/louis/gui/vscode.nix
+++ home/louis/gui/vscode.nix
@@ -5,20 +5,29 @@
 }: {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
+    package = (pkgs.vscode.override {isInsiders = true;}).overrideAttrs (oldAttrs: rec {
+      src = builtins.fetchTarball {
+        url = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/9621add46007f7a1ab37d1fce9bcdcecca62aeb0/code-insider-x64-1703050604.tar.gz";
+        sha256 = "017630xgr64qjva73imb56fcqr858xfcsbdgq97akawlxf1ydm5a";
+      };
+      version = "latest";
+
+      buildInputs = oldAttrs.buildInputs ++ [pkgs.krb5];
+    });
     enableExtensionUpdateCheck = false;
     enableUpdateCheck = false;
     extensions = with pkgs.vscode-extensions;
       [
         mkhl.direnv
         jnoortheen.nix-ide
-        ms-vscode.cpptools
-        ms-dotnettools.csharp
-        ms-python.python
-        ms-vscode.cmake-tools
+        #ms-vscode.cpptools
+        #ms-dotnettools.csharp
+        #ms-python.python
+        #ms-vscode.cmake-tools
         nvarner.typst-lsp
         tomoki1207.pdf
         github.copilot
+        github.copilot-chat
       ]
       ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
         {
