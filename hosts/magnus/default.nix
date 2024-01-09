78838087fafd75d3ee34d78ebf3403eb896f9722
Louis THEVENET <louis.tvnt@gmail.Com>
2024-01-09 18:05:28 +0100

    enables openGL

--- hosts/magnus/default.nix
+++ hosts/magnus/default.nix
@@ -13,4 +13,11 @@
   networking.hostName = "magnus";
   services.xserver.videoDrivers = ["nvidia"];
   boot.binfmt.emulatedSystems = ["aarch64-linux"]; # allows building iso for arm devices
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
 }
