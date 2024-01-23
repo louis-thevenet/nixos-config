88d2887ec2ac644a44e62e087464d9cca184f185
Louis THEVENET <louis.tvnt@gmail.com>
2024-01-23 16:11:53 +0100

    feat: enables hyprland

--- hosts/hircine/default.nix
+++ hosts/hircine/default.nix
@@ -13,8 +13,8 @@
     xserver = {
       enable = true;
       layout = "fr";
-      desktopManager.gnome = {
-        enable = true;
+       desktopManager.gnome = {
+         enable = true;
       };
       displayManager.gdm = {
         enable = true;
@@ -23,4 +23,29 @@
   };
 
   networking.hostName = "hircine";
+
+  # XDG Portals
+  programs.hyprland = {
+    enable = true;
+  };
+
+  # XDG Portals
+  xdg = {
+    portal = {
+      enable = true;
+    };
+  };
+
+  environment.systemPackages = [
+    pkgs.xdg-utils # xdg-open
+  ];
+
+  # environment.sessionVariables = {
+  #   # NIXOS_OZONE_WL = "1"; disable for now since vs code is broken
+  #   SDL_VIDEODRIVER = "wayland";
+  #   _JAVA_AWT_WM_NONREPARENTING = "1";
+  #   CLUTTER_BACKEND = "wayland";
+  #   WLR_RENDERER = "vulkan";
+  #   QT_QPA_PLATFORM = "wayland";
+  # };
 }
