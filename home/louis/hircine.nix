c3461cad4e53520c9c821adcc9df58038d7e4ddf
Louis THEVENET <louis.tvnt@gmail.com>
2024-01-24 13:43:06 +0100

    feat: configure monitors and font for hircine

--- home/louis/hircine.nix
+++ home/louis/hircine.nix
@@ -13,5 +13,28 @@
     ./cli
     ./optional/gnome.nix
     ./optional/hyprland
+    ./optional/wayland-wm
   ];
+  monitors = [
+    {
+      name = "eDP-1";
+      width = 1920;
+      height = 1080;
+      x = 0;
+      workspace = "1";
+      primary = true;
+    }
+  ];
+
+  fontProfiles = {
+    enable = true;
+    monospace = {
+      family = "FiraCode Nerd Font";
+      package = pkgs.nerdfonts.override {fonts = ["FiraCode"];};
+    };
+    regular = {
+      family = "Fira Sans";
+      package = pkgs.fira;
+    };
+  };
 }
