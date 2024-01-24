371ed28c8ce93ae095509a292560b5a3f7a7be25
Louis THEVENET <louis.tvnt@gmail.Com>
2024-01-24 22:26:35 +0100

    feat(magnus): adds monitors config + fontprofiles

--- home/louis/magnus.nix
+++ home/louis/magnus.nix
@@ -13,5 +13,36 @@
     ./dev
     ./optional/gnome.nix
     ./optional/hyprland
+    ./optional/wayland-wm
   ];
+
+  monitors = [
+    {
+      name = "HDMI-A-1";
+      width = 1920;
+      height = 1080;
+      x = 0;
+      workspace = "1";
+    }
+    {
+      name = "DP-2";
+      width = 2560;
+      height = 1440;
+      x = 1920;
+      workspace = "1";
+      primary = true;
+    }
+  ];
+
+  fontProfiles = {
+    enable = true;
+    monospace = {
+      family = "FiraCode Nerd Font";
+      package = pkgs.nerdfonts.override {fonts = ["FiraCode"];};
+    };
+    regular = {
+      family = "Fira Sans";
+      package = pkgs.fira;
+    };
+  };
 }
