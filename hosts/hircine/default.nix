0d63100a6e0daba3e01b4b390442490415945d79
Louis Thevenet <louis.tvnt@gmail.com>
2024-01-30 23:07:09 +0100

    feat: adds nvidia driver working under wayland

--- hosts/hircine/default.nix
+++ hosts/hircine/default.nix
@@ -1,6 +1,6 @@
 {
-  pkgs,
-  inputs,
+  lib,
+  config,
   ...
 }: {
   imports = [
@@ -9,10 +9,12 @@
     ../common/users/louis
     ../optional/gnome.nix
   ];
+  networking.hostName = "hircine";
+
   services = {
     xserver = {
       enable = true;
-      layout = "fr";
+      xkb.layout = "fr";
       desktopManager.gnome = {
         enable = true;
       };
@@ -22,29 +24,39 @@
     };
   };
 
-  networking.hostName = "hircine";
-
   programs.hyprland = {
     enable = true;
   };
 
-  # XDG Portals
-  xdg = {
-    portal = {
-      enable = true;
-    };
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
   };
 
-  environment.systemPackages = [
-    pkgs.xdg-utils # xdg-open
-  ];
+  # NVIDIA drivers are unfree.
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "nvidia-x11"
+      "nvidia-settings"
+    ];
+  # Tell Xorg to use the nvidia driver
+  services.xserver.videoDrivers = ["nvidia"];
 
-  # environment.sessionVariables = {
-  #   # NIXOS_OZONE_WL = "1"; disable for now since vs code is broken
-  #   SDL_VIDEODRIVER = "wayland";
-  #   _JAVA_AWT_WM_NONREPARENTING = "1";
-  #   CLUTTER_BACKEND = "wayland";
-  #   WLR_RENDERER = "vulkan";
-  #   QT_QPA_PLATFORM = "wayland";
-  # };
+  hardware.nvidia = {
+    # Modesetting is needed for most wayland compositors
+    modesetting.enable = true;
+
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    # The default is true but I'm changing it to false so reverse prime sync works
+    open = true;
+
+    # Enable the nvidia settings menu
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
