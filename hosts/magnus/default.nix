1fe0aab98e1f57b3da911aaa21676f71e6f5c8df
Louis Thevenet <louis.tvnt@gmail.com>
2024-01-30 22:59:46 +0100

    feat: nvdidia working under wayland

--- hosts/magnus/default.nix
+++ hosts/magnus/default.nix
@@ -1,6 +1,8 @@
 {
   pkgs,
+  lib,
   inputs,
+  config,
   ...
 }: {
   imports = [
@@ -9,16 +11,8 @@
     ../common/users/louis
     ../optional/gnome.nix
   ];
-
   networking.hostName = "magnus";
   boot.binfmt.emulatedSystems = ["aarch64-linux"]; # allows building iso for arm devices
-
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
 
   services = {
     xserver = {
@@ -33,8 +27,42 @@
     };
   };
 
-  programs.hyprland = {
+  # Enable OpenGL
+  hardware.opengl = {
     enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # NVIDIA drivers are unfree.
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "nvidia-x11"
+      "nvidia-settings"
+    ];
+  # Tell Xorg to use the nvidia driver
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    # Modesetting is needed for most wayland compositors
+    modesetting.enable = true;
+
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    # The default is true but I'm changing it to false so reverse prime sync works
+    open = true;
+
+    # Enable the nvidia settings menu
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  programs.hyprland = {
+    # or wayland.windowManager.hyprland
+    enable = true;
+    xwayland.enable = true;
   };
 
   # XDG Portals
