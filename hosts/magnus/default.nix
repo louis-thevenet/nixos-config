267f14e6cc741504f613da49a879c3046bb6ea9b
Louis Thevenet <louis.tvnt@gmail.com>
2024-01-31 09:56:46 +0100

    fix: redundant options + style

--- hosts/magnus/default.nix
+++ hosts/magnus/default.nix
@@ -27,9 +27,7 @@
   };
 
   programs.hyprland = {
-    # or wayland.windowManager.hyprland
     enable = true;
-    xwayland.enable = true;
   };
 
   # Enable OpenGL
@@ -45,23 +43,15 @@
       "nvidia-x11"
       "nvidia-settings"
     ];
-  # Tell Xorg to use the nvidia driver
+
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
     # Modesetting is needed for most wayland compositors
     modesetting.enable = true;
-
-    # Use the open source version of the kernel module
-    # Only available on driver 515.43.04+
-    # The default is true but I'm changing it to false so reverse prime sync works
     open = true;
-
-    # Enable the nvidia settings menu
     nvidiaSettings = true;
-
     package = config.boot.kernelPackages.nvidiaPackages.production;
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   # XDG Portals
