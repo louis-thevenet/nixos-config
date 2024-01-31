82f7b65c4e22f101a59905954007f72cd10d849c
Louis Thevenet <louis.tvnt@gmail.com>
2024-01-31 09:56:57 +0100

    style

--- hosts/hircine/default.nix
+++ hosts/hircine/default.nix
@@ -47,16 +47,8 @@
   hardware.nvidia = {
     # Modesetting is needed for most wayland compositors
     modesetting.enable = true;
-
-    # Use the open source version of the kernel module
-    # Only available on driver 515.43.04+
-    # The default is true but I'm changing it to false so reverse prime sync works
     open = true;
-
-    # Enable the nvidia settings menu
     nvidiaSettings = true;
-
     package = config.boot.kernelPackages.nvidiaPackages.production;
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
