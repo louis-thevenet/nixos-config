3de36a4e70c54ced59e56c14e8a38a19a02daebb
Louis Thevenet <louis.tvnt@gmail.com>
2024-02-01 11:01:43 +0100

    feat(magnus): nvidia driver : production -> stable

--- hosts/magnus/default.nix
+++ hosts/magnus/default.nix
@@ -53,7 +53,7 @@
     modesetting.enable = true;
     open = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   # XDG Portals
