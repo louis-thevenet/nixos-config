9c372cc1ba20a2b332785186d0ea1d386cced615
Louis Thevenet <louis.tvnt@gmail.com>
2024-02-24 17:01:53 +0100

    feat(users): update default password

--- hosts/common/users/louis/default.nix
+++ hosts/common/users/louis/default.nix
@@ -21,13 +21,13 @@ in {
 
   system.activationScripts.createDefaultPasswordFile = ''
     if [ ! -f ${passwordFile} ]; then
-      echo -e "\e[31m${passwordFile} doesn't exist, creating it. Password is \"tmp\" \e[0m"
+      echo -e "\e[31m${passwordFile} doesn't exist, creating it. Password is \"tmppwd\" \e[0m"
 
-      mkdir -p $(dirname ${passwordFile}) && touch ${passwordFile}
-      echo "$y$j9T$fRcgMeFa9VRjBE0yitrh70$DR1AoWRerynpYpLpW5Ncmi8KYFFPlJfqRgUzDbVgnT1" > ${passwordFile}
+      mkdir -p $(dirname ${passwordFile})
+      echo "$y$j9T$frTeOx1XSSp87P5svRYjb.$7Hx9d2pJs8UYYElzoeukYiB6jU.UPK5ZXnj0VgkqHg5" > ${passwordFile}
 
       # what if the update users script runs before this one? Let's change the password in case
-      yes tmp | passwd louis
+      yes tmppwd | passwd louis
     fi
   '';
   security.pam.services.swaylock = {};
