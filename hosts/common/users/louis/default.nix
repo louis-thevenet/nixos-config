fe93bd020bdda6223ad4f5865f17bdb054a371e3
Louis Thevenet <louis.tvnt@gmail.com>
2024-02-24 15:08:20 +0100

    feat(user): default password if secret not set

--- hosts/common/users/louis/default.nix
+++ hosts/common/users/louis/default.nix
@@ -2,7 +2,9 @@
   pkgs,
   config,
   ...
-}: {
+}: let
+  passwordFile = config.sops.secrets.sys-passphrase.path;
+in {
   users = {
     mutableUsers = false;
     users = {
@@ -12,9 +14,21 @@
         extraGroups = ["networkmanager" "wheel"];
         shell = pkgs.zsh;
         home = "/home/louis";
-        hashedPasswordFile = config.sops.secrets.sys-passphrase.path;
+        hashedPasswordFile = passwordFile;
       };
     };
   };
+
+  system.activationScripts.createDefaultPasswordFile = ''
+    if [ ! -f ${passwordFile} ]; then
+      echo -e "\e[31m${passwordFile} doesn't exist, creating it. Password is \"tmp\" \e[0m"
+
+      mkdir -p ''${dirname ${passwordFile}} && touch ${passwordFile}
+      echo "$y$j9T$fRcgMeFa9VRjBE0yitrh70$DR1AoWRerynpYpLpW5Ncmi8KYFFPlJfqRgUzDbVgnT1" > ${passwordFile}
+
+      # what if the update users script runs before this one? Let's change the password in case
+      yes tmp | passwd louis
+    fi
+  '';
   security.pam.services.swaylock = {};
 }
