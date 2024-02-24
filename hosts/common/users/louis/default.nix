a9dabbd44aac69ab8030f5b2000ba56235476bfe
Louis Thevenet <louis.tvnt@gmail.com>
2024-02-24 17:39:59 +0100

    fix(users): fix pwd hash

--- hosts/common/users/louis/default.nix
+++ hosts/common/users/louis/default.nix
@@ -21,13 +21,13 @@ in {
 
   system.activationScripts.createDefaultPasswordFile = ''
     if [ ! -f ${passwordFile} ]; then
-      echo -e "\e[31m${passwordFile} doesn't exist, creating it. Password is \"tmppwd\" \e[0m"
+      echo -e "\e[31m${passwordFile} doesn't exist, creating it. Password is \"tmp\" \e[0m"
 
       mkdir -p $(dirname ${passwordFile})
-      echo "$y$j9T$frTeOx1XSSp87P5svRYjb.$7Hx9d2pJs8UYYElzoeukYiB6jU.UPK5ZXnj0VgkqHg5" > ${passwordFile}
+      echo "$6$KJkipd5jIyIQSA3L$WwEKtx69GoKsMucjvLEfmZCm87y62sDDOl1JEslXmHLlNeql8zKiGmGVYBF3Q4cj/qJSR9OA2tdyN4hK4jjVc." > ${passwordFile}
 
       # what if the update users script runs before this one? Let's change the password in case
-      yes tmppwd | passwd louis
+      yes tmp | passwd louis
     fi
   '';
   security.pam.services.swaylock = {};
