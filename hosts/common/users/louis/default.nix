d6e75702d6535a7ca3a70d200de2bd49f354cf24
Louis Thevenet <louis.tvnt@gmail.com>
2024-02-24 21:01:34 +0100

    feat(sops): adds hircine and removes users password script

--- hosts/common/users/louis/default.nix
+++ hosts/common/users/louis/default.nix
@@ -19,16 +19,5 @@ in {
     };
   };
 
-  system.activationScripts.createDefaultPasswordFile = ''
-    if [ ! -f ${passwordFile} ]; then
-      echo -e "\e[31m${passwordFile} doesn't exist, creating it. Password is \"tmp\" \e[0m"
-
-      mkdir -p $(dirname ${passwordFile})
-      echo "$6$KJkipd5jIyIQSA3L$WwEKtx69GoKsMucjvLEfmZCm87y62sDDOl1JEslXmHLlNeql8zKiGmGVYBF3Q4cj/qJSR9OA2tdyN4hK4jjVc." > ${passwordFile}
-
-      # what if the update users script runs before this one? Let's change the password in case
-      yes tmp | passwd louis
-    fi
-  '';
   security.pam.services.swaylock = {};
 }
