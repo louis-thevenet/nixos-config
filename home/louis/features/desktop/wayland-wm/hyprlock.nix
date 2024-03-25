8cdd59f8083b997dbedaba8a60946b0e75aa50c3
Louis Thevenet <louis.tvnt@gmail.com>
2024-03-25 10:54:57 +0100

    feat: add blur to hyprlock screen

--- home/louis/features/desktop/wayland-wm/hyprlock.nix
+++ home/louis/features/desktop/wayland-wm/hyprlock.nix
@@ -38,9 +38,15 @@ in {
     }
 
     background {
-        color = rgba(130F0F77)
-        blur_size = 5
-        blur_passes = 4
+           # all these options are taken from hyprland, see https://wiki.hyprland.org/Configuring/Variables/#blur for explanations
+    path = screenshot
+    blur_passes = 3 # 0 disables blurring
+    blur_size = 7
+    noise = 0.0117
+    contrast = 0.8916
+    brightness = 1 # 0.8172
+    vibrancy = 0.1696
+    vibrancy_darkness = 0.0
     }
 
 
