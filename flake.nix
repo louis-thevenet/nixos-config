5183d4baaef7b2ac3e528724939f9e45ca6f814d
Louis Thevenet <louis.tvnt@gmail.com>
2024-04-02 16:42:47 +0200

    feat: update hyprland to 0.38.0

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     nix-colors.url = "github:misterio77/nix-colors";
 
     hyprland = {
-      url = "github:hyprwm/Hyprland/v0.37.1";
+      url = "github:hyprwm/Hyprland/v0.38.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
