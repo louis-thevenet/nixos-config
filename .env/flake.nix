6adc04b3c4798de9f4c5a947f94014f481650a45
Louis Thevenet <louis.tvnt@gmail.com>
2024-06-02 18:26:37 +0200

    build: add treefmt + reformat

--- .env/flake.nix
+++ .env/flake.nix
@@ -1,33 +1,37 @@
 {
   description = "A flake-based Nix development environment";
-
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
   outputs = {
     self,
     nixpkgs,
   }: let
-    supportedSystems = ["x86_64-linux" "aarch64-linux"];
-    forEachSupportedSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
-          pkgs = import nixpkgs {inherit system;};
-        });
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {system = system;};
   in {
-    devShells = forEachSupportedSystem ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          nil
-          alejandra
-          python311Packages.nix-prefetch-github
-          home-manager
-          nixos-generators
-          nix-du
-          graphviz
-          sops
-          age
-        ];
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      packages = with pkgs; [
+        nil
+        nixpkgs-fmt
+        python311Packages.nix-prefetch-github
+        home-manager
+        nixos-generators
+        nix-du
+        graphviz
+        sops
+        age
+      ];
+    };
+
+    # Add your auto-formatters here.
+    # cf. https://numtide.github.io/treefmt/
+    treefmt.config = {
+      projectRootFile = "flake.nix";
+      programs = {
+        nixpkgs-fmt.enable = true;
       };
-    });
+    };
   };
 }
