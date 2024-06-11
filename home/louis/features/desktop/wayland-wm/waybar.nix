44fa64808d9e99c4f1774a8f5f46849471bcc1dd
Louis Thevenet <louis.tvnt@gmail.com>
2024-06-11 14:06:21 +0200

    fix: fix waybar config

--- home/louis/features/desktop/wayland-wm/waybar.nix
+++ home/louis/features/desktop/wayland-wm/waybar.nix
@@ -276,7 +276,7 @@ in {
     # x y -> vertical, horizontal
     # x y z -> top, horizontal, bottom
     # w x y z -> top, right, bottom, left
-    style =
+    style = with config.lib.stylix;
       # css
       ''
 
@@ -294,15 +294,40 @@ in {
 
         window#waybar.top {
           padding: 0;
+          background-color: #${colors.base00};
+          border: 2px solid #${colors.base0C};
           border-radius: 10px;
         }
 
+        window#waybar {
+          color: #${colors.base05};
+        }
+
         #workspaces button {
+          background-color: #${colors.base01};
+          color: #${colors.base05};
           margin: 4px;
           border-radius: 10px;
         }
 
+        #workspaces button.special {
+          background-color: #${colors.base0B};
+          color: #${colors.base00};
+        }
+
+        #workspaces button.hidden {
+          background-color: #${colors.base00};
+          color: #${colors.base04};
+        }
+        #workspaces button.focused,
+        #workspaces button.active {
+          background-color: #${colors.base0A};
+          color: #${colors.base00};
+        }
+
         #clock {
+          background-color: #${colors.base0C};
+          color: #${colors.base00};
           padding-left: 15px;
           padding-right: 15px;
           margin-top: 0;
@@ -311,6 +336,8 @@ in {
         }
 
         #custom-menu {
+          background-color: #${colors.base0C};
+          color: #${colors.base00};
           padding-left: 15px;
           padding-right: 22px;
           margin-left: 0;
@@ -320,12 +347,17 @@ in {
           border-radius: 10px;
         }
         #custom-hostname {
+          background-color: #${colors.base0C};
+          color: #${colors.base00};
           padding-left: 15px;
           padding-right: 18px;
           margin-right: 0;
           margin-top: 0;
           margin-bottom: 0;
           border-radius: 10px;
+        }
+        #tray {
+          color: #${colors.base05};
         }
       '';
   };
