533ac63f1db72e03aa33dd870bdffa9146023003
Louis Thevenet <louis.tvnt@gmail.com>
2024-06-11 14:00:17 +0200

    feat: add cursor theme

--- home/louis/global/stylix.nix
+++ home/louis/global/stylix.nix
@@ -10,7 +10,12 @@
       url = "https://images.pexels.com/photos/167698/pexels-photo-167698.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
       sha256 = "sha256-/Pw6zZ41isjbUwsaFOt2YWhE7oD8D6kNdLsaGtUdBrI=";
     };
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/standardized-light.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/selenized-light.yaml";
+    cursor = {
+      package = pkgs.numix-cursor-theme;
+      name = "Numix-Cursor-Light";
+      size = 22;
+    };
     fonts = {
       monospace = {
         name = "FiraCode Nerd Font";
