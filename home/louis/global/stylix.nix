8c70c83212694314db9a4833235870fee1dbed82
Louis Thevenet <louis.tvnt@gmail.com>
2024-06-19 14:10:22 +0200

    fix: stylix wallpaper dimension

--- home/louis/global/stylix.nix
+++ home/louis/global/stylix.nix
@@ -7,8 +7,8 @@
   stylix = {
     enable = true;
     image = pkgs.fetchurl {
-      url = "https://images.pexels.com/photos/167698/pexels-photo-167698.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-      sha256 = "sha256-/Pw6zZ41isjbUwsaFOt2YWhE7oD8D6kNdLsaGtUdBrI=";
+      url = "https://images.pexels.com/photos/167698/pexels-photo-167698.jpeg?auto=compress&cs=tinysrgb&w=2560&h=1920&dpr=1";
+      sha256 = "sha256-z/fLJ/0mSLCnhseg5NFWqlwAWnJOJkRpju6NCSiu9b4=";
     };
     base16Scheme = "${pkgs.base16-schemes}/share/themes/selenized-light.yaml";
     cursor = {
