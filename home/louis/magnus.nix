50f208b39fae04bbf46882b8152629631941f866
Louis Thevenet <louis.tvnt@gmail.com>
2024-07-09 07:44:29 +0200

    fix: delete old font profile

--- home/louis/magnus.nix
+++ home/louis/magnus.nix
@@ -54,16 +54,4 @@
       primary = true;
     }
   ];
-
-  fontProfiles = {
-    enable = true;
-    monospace = {
-      family = "FiraCode Nerd Font";
-      package = pkgs.nerdfonts.override {fonts = ["FiraCode"];};
-    };
-    regular = {
-      family = "Fira Sans";
-      package = pkgs.fira;
-    };
-  };
 }
