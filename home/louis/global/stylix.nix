a15a0dba860ca9104bbfe71d0b8382b2f627b29d
Louis Thevenet <louis.tvnt@gmail.com>
2024-07-24 10:02:38 +0200

    feat(stylix): change theme

--- home/louis/global/stylix.nix
+++ home/louis/global/stylix.nix
@@ -10,7 +10,7 @@
       url = "https://images.pexels.com/photos/167698/pexels-photo-167698.jpeg?auto=compress&cs=tinysrgb&w=2560&h=1920&dpr=1";
       sha256 = "sha256-z/fLJ/0mSLCnhseg5NFWqlwAWnJOJkRpju6NCSiu9b4=";
     };
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/selenized-light.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/equilibrium-gray-light.yaml";
     cursor = {
       package = pkgs.numix-cursor-theme;
       name = "Numix-Cursor-Light";
