15daeb40927d814ba8ae41b4ef39ad3354b09e07
Louis Thevenet <louis.tvnt@gmail.com>
2024-08-04 20:02:21 +0200

    feat(hircine): enable udisk2 (for calibre)

--- hosts/hircine/default.nix
+++ hosts/hircine/default.nix
@@ -29,6 +29,7 @@
 
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  services.udisks2.enable = true;
 
   programs.hyprland = {
     enable = true;
