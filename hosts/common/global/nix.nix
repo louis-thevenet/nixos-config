82a9ea300240efa85b50bc9b96e92c9bf80b7a38
Louis Thevenet <louis.tvnt@gmail.com>
2024-08-25 09:26:58 +0200

    feat: add new substituters

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -11,9 +11,27 @@
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
 
     settings = {
-      experimental-features = "nix-command flakes";
+      experimental-features = ["nix-command" "flakes" "ca-derivations"];
       auto-optimise-store = true;
-      cores = 4;
+
+      trusted-users = ["louis"];
+
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+
+        "https://nix-ai-stuff.cachix.org"
+        "https://cuda-maintainers.cachix.org"
+        "https://ai.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+
+        "nix-ai-stuff.cachix.org-1:WlUGeVCs26w9xF0/rjyg32PujDqbVMlSHufpj1fqix8="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
+      ];
     };
   };
 
