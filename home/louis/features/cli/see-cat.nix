1f95a925d7d9c7ad78fc65225ee1f4ff6a5b4d82
Louis Thevenet <louis.tvnt@gmail.com>
2024-09-15 08:21:14 +0200

    see-cat: 0.5.0 -> 0.8.1

--- home/louis/features/cli/see-cat.nix
+++ home/louis/features/cli/see-cat.nix
@@ -3,34 +3,33 @@
   fetchFromGitHub,
   rustPlatform,
   pkg-config,
-  openssl,
-  perl,
 }:
 rustPlatform.buildRustPackage rec {
-  pname = "see";
-  version = "0.5.0";
+  pname = "see-cat";
+  version = "0.8.1";
 
   src = fetchFromGitHub {
     owner = "guilhermeprokisch";
-    repo = pname;
+    repo = "see";
     rev = "v${version}";
-    hash = "sha256-4BVnM3SYDHsfjMsrzdFNuQ2jF5nQKa1+QH4Jf4HsXvg=";
+    hash = "sha256-VCUrPCaG2fKp9vpFLzNLcfCBu2NiwdY2+bo1pd7anZY=";
   };
 
-  cargoHash = "sha256-kQkrn7s5UuNSHkAM+hk8mFWDpnHtt8UcyyXGr1m4xkg=";
+  cargoHash = "sha256-lfpJ40QpZ9eQhDFJjLwiDU5DmaYAqCh5iJSjZ+jj+kk=";
 
   nativeBuildInputs = [
     pkg-config
-    perl
-  ];
-
-  buildInputs = [
-    openssl
   ];
 
   meta = {
-    description = "Cute cat(1) for the terminal with advanced code viewing, Markdown rendering, 🌳 tree-sitter syntax highlighting, and more.";
-    longDescription = "see is a powerful file visualization tool for the terminal, offering advanced code viewing capabilities, Markdown rendering, and more. It provides syntax highlighting, emoji support, and image rendering capabilities, offering a visually appealing way to view various file types directly in your console.";
+    description = "Cute cat(1) for the terminal";
+    longDescription = ''
+      see is a powerful file visualization tool for the terminal, offering
+      advanced code viewing capabilities, Markdown rendering, and
+      more. It provides syntax highlighting, emoji support, and image
+      rendering capabilities, offering a visually appealing way to view
+      various file types directly in your console.
+    '';
     homepage = "https://github.com/guilhermeprokisch/see";
     license = lib.licenses.mit;
     mainProgram = "see";
