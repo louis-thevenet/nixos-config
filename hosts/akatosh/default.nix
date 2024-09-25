ec554e3ce631d8be402340e3640a1517ed888e76
Louis Thevenet <louis.tvnt@gmail.com>
2024-09-25 10:01:14 +0200

    steam(akatosh): enable

--- hosts/akatosh/default.nix
+++ hosts/akatosh/default.nix
@@ -32,7 +32,7 @@
     };
     displayManager.sddm.enable = true;
   };
-
+  programs.steam.enable = true;
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
   services.udisks2.enable = true;
