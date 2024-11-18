a1638faf2a77f6ed3e8fbad1a11acdd8b433518f
Louis Thevenet <louis.tvnt@gmail.com>
2024-11-18 10:16:23 +0100

    stylix: use google themes

--- home/louis/features/desktop/stylix-specialisation.nix
+++ home/louis/features/desktop/stylix-specialisation.nix
@@ -12,12 +12,12 @@ in
   specialisation = mkIf cfg.stylix.enable {
     light.configuration = {
       stylix.image = ../../../../hosts/common/optional/background_light.png;
-      stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/solarized-light.yaml";
+      stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/google-light.yaml";
     };
 
     dark.configuration = {
       stylix.image = ../../../../hosts/common/optional/background_dark.jpeg;
-      stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/solarized-dark.yaml";
+      stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/google-dark.yaml";
     };
   };
 }
