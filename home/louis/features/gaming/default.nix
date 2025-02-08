30f5dba7054e5ecb233622f2ca8ba6fb7d01f892
Louis Thevenet <louis.tvnt@gmail.com>
2025-02-08 09:56:30 +0100

    vintagestory: init

--- home/louis/features/gaming/default.nix
+++ home/louis/features/gaming/default.nix
@@ -9,15 +9,13 @@ let
   cfg = config.home-config;
 in
 {
+  nixpkgs.config.permittedInsecurePackages = [
+    "dotnet-runtime-7.0.20"
+  ];
   home.packages = mkIf cfg.gaming.enable (
     with pkgs;
     [
-      # prismlauncher
-      # lutris
-      # wine
-      # heroic
-      # goverlay
-      # mangohud
+      vintagestory
     ]
   );
 }
