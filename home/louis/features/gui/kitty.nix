e895c6d3186aae83462385b46e92695ac8989ac7
Louis Thevenet <louis.tvnt@gmail.com>
2025-02-21 11:28:45 +0100

    kitty: remove old theme

--- home/louis/features/gui/kitty.nix
+++ home/louis/features/gui/kitty.nix
@@ -10,7 +10,6 @@ in
 {
   programs.kitty = mkIf cfg.kitty.enable {
     enable = true;
-    theme = "Solarized Light";
     settings = {
       scrollback_lines = 4000;
       scrollback_pager_history_size = 2048;
