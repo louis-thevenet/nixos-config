36b64ca2a46c54a842bc27ad982e9987f3e37858
Louis Thevenet <louis.tvnt@gmail.com>
2025-03-12 20:06:30 +0100

    niri: more config

--- hosts/akatosh/default.nix
+++ hosts/akatosh/default.nix
@@ -9,7 +9,6 @@
     ../common/optional/impermanence-disko.nix
     ../common/optional/ollama.nix
     ../common/optional/niri.nix
-    # ../common/optional/hyprland.nix
     ../common/optional/tlp.nix
     ../common/optional/kanata.nix
     ../common/optional/xdg.nix
