e4efdf203a856524fefa18d5085173bf5c6b06e9
Louis Thevenet <louis.tvnt@gmail.com>
2025-04-26 14:32:39 +0200

    users: add root backup user

--- hosts/common/users/louis/default.nix
+++ hosts/common/users/louis/default.nix
@@ -21,6 +21,14 @@ in
         home = "/home/louis";
         hashedPasswordFile = passwordFile;
       };
+      root = {
+        # so, this may look like a security issue. I'm publicly showing the hash of my password. However:
+        # 1. this password is extremely robust (~2000 bits of entropy)
+        # 2. it is not used anywhere else
+        # 3. it cannot be used to login over ssh
+        # considering the odds of someone stealing my computer AND knowing how to crack this, I feel safe enough to put it here
+        hashedPassword = "$6$yZ3vsEdcCJUTFq4l$0z1DIXaHUPZ/WjHW1qgqa3XZkAZraq8D7KJzrfbuvEx2hoRn.c/tBLE/fcN71yfVv4hnB6SpsnHYqLr1uCX7m.";
+      };
     };
   };
 
