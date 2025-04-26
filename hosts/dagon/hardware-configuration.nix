9d23d21b2f6ec901bd4b5f717da1e3ba613dccc8
Louis Thevenet <louis.tvnt@gmail.com>
2025-04-26 11:50:50 +0200

    dagon: remove boot options

--- hosts/dagon/hardware-configuration.nix
+++ hosts/dagon/hardware-configuration.nix
@@ -8,13 +8,6 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot = {
-    initrd.availableKernelModules = [ ];
-    initrd.kernelModules = [ ];
-    kernelModules = [ ];
-    extraModulePackages = [ ];
-  };
-
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
     fsType = "ext4";
