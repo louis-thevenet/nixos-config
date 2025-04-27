9b959135689c49b93ebc240f28a124404518d308
Louis Thevenet <louis.tvnt@gmail.com>
2025-04-27 09:21:54 +0200

    kanata: I might have plugged that into a different USB port

--- hosts/common/optional/kanata.nix
+++ hosts/common/optional/kanata.nix
@@ -23,8 +23,8 @@ _: {
         devices = [
           # Replace the paths below with the appropriate device paths for your setup.
           # Use `ls /dev/input/by-path/` to find your keyboard devices.
-          "/dev/input/by-path/pci-0000:00:14.0-usbv2-0:10:1.0-event-kbd"
-          "/dev/input/by-path/pci-0000:00:14.0-usbv2-0:10:1.2-event-kbd"
+          "/dev/input/by-path/pci-0000:00:14.0-usb-0:11:1.0-event-kbd"
+          "/dev/input/by-path/pci-0000:00:14.0-usb-0:11:1.2-event-kbd"
           "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
         ];
         extraDefCfg = "process-unmapped-keys yes";
