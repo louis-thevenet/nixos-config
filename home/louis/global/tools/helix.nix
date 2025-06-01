226ead952695a87bd2017bf3172fd170fe827951
Louis Thevenet <louis.tvnt@gmail.com>
2025-06-01 08:15:45 +0200

    helix: add helix-gpt lsp

--- home/louis/global/tools/helix.nix
+++ home/louis/global/tools/helix.nix
@@ -1,31 +1,17 @@
 {
+  config,
   lib,
   inputs,
   pkgs,
   ...
 }:
-# let
-
-#   copilot = pkgs.writeShellScriptBin "copilot" "${lib.getExe pkgs.nodejs} ${pkgs.vimPlugins.copilot-vim}/dist/language-server.js";
-#   helix-copilot = pkgs.symlinkJoin {
-#     name = "helix";
-#     paths = [
-#       inputs.helix.packages.${pkgs.system}.helix
-#     ];
-#     buildInputs = [ pkgs.makeWrapper ];
-#     runtimeInputs = [
-#       copilot
-#     ];
-#     postBuild = ''
-#       wrapProgram $out/bin/hx --add-flags "-a"
-#     '';
-#   };
-# in
 {
+  sops.secrets.copilot-api-key = {
+    sopsFile = ../../../../hosts/common/secrets.yaml;
+  };
 
   programs.helix = {
     enable = true;
-    # package = helix-copilot;
     defaultEditor = true;
 
     settings = {
@@ -55,7 +41,6 @@
         cursorcolumn = true;
         soft-wrap.enable = true;
         text-width = 110;
-
         statusline = {
           left = [
             "mode"
@@ -78,12 +63,6 @@
       };
 
       keys = {
-
-        # insert = {
-        #   "C-w" = "copilot_apply_completion";
-        #   "C-e" = "copilot_show_completion";
-        # };
-
         normal = {
           "C-y" =
             let
@@ -98,18 +77,14 @@
               ":set mouse false"
               ":set mouse true"
             ];
-          # "C-e" = "copilot_toggle_auto_render";
           "h" = "no_op";
           "j" = "no_op";
           "k" = "no_op";
           "l" = "no_op";
-
           "!" = "no_op";
-
           "ret" = "goto_word";
           backspace = {
-            # Vault-tasks stuff
-            t = "@i- [ ] today ";
+            # Vault-tasks stuff t = "@i- [ ] today ";
             n = "@i- [ ] ";
             # Get current time
             d = ":insert-output ${lib.getExe' pkgs.coreutils "date"} +'## %H:%M:%S'";
@@ -118,28 +93,17 @@
               "open_below"
               ":insert-output ${lib.getExe' pkgs.coreutils "date"} +'## %H:%M:%S'"
             ];
-
             y = ":yank-diagnostic";
           };
-
-          x = [
-            "extend_line_below"
-          ];
-          X = [
-            "extend_line_above"
-          ];
+          x = [ "extend_line_below" ];
+          X = [ "extend_line_above" ];
         };
         select = {
-          x = [
-            "extend_line_below"
-          ];
-          X = [
-            "extend_line_above"
-          ];
+          x = [ "extend_line_below" ];
+          X = [ "extend_line_above" ];
         };
       };
     };
-
     languages = {
       language = [
         {
@@ -148,6 +112,7 @@
             "bash-language-server"
             "gpt"
             "typos"
+            "wakatime"
           ];
           formatter = {
             command = "${pkgs.shfmt}/bin/shfmt";
@@ -159,11 +124,42 @@
           };
         }
         {
+          name = "fish";
+          language-servers = [
+            "fish"
+            "gpt"
+            "typos"
+            "wakatime"
+          ];
+        }
+        {
+          name = "java";
+          language-servers = [
+            "jdtls"
+            "gpt"
+            "typos"
+            "wakatime"
+          ];
+          auto-format = true;
+        }
+        {
+          name = "javascript";
+          language-servers = [
+            "typescript-language-server"
+            "gpt"
+            "typos"
+            "wakatime"
+          ];
+          auto-format = true;
+        }
+        {
           name = "markdown";
           language-servers = [
-            "markdown-oxide"
-            "wakatime"
             "ltex-ls"
+            "markdown-oxide"
+            "gpt"
+            "typos"
+            "wakatime"
           ];
           formatter = {
             command = lib.getExe pkgs.nodePackages.prettier;
@@ -175,37 +171,16 @@
           auto-format = true;
         }
         {
-          name = "typst";
+          name = "nix";
           language-servers = [
-            "tinymist"
+            "nixd"
+            "gpt"
+            "typos"
             "wakatime"
           ];
-          formatter = {
-            command = lib.getExe pkgs.typstyle;
-          };
+          formatter.command = lib.getExe pkgs.pkgs.nixfmt-rfc-style;
           auto-format = true;
         }
-        {
-          name = "python";
-          language-servers = [
-            "python-lsp"
-            "wakatime"
-            "typos"
-          ];
-          formatter = {
-            command = lib.getExe pkgs.python312Packages.yapf;
-          };
-          auto-format = true;
-        }
-        {
-          name = "rust";
-          language-servers = [
-            "rust-analyzer"
-            "wakatime"
-            "typos"
-          ];
-        }
-
         {
           name = "ocaml";
           file-types = [
@@ -214,6 +189,8 @@
           ];
           language-servers = [
             "ocaml-lsp"
+            "gpt"
+            "typos"
             "wakatime"
           ];
           formatter = {
@@ -227,163 +204,123 @@
           auto-format = true;
         }
         {
+          name = "python";
+          language-servers = [
+            "python-lsp"
+            "gpt"
+            "typos"
+            "wakatime"
+          ];
+          formatter = {
+            command = lib.getExe pkgs.python312Packages.yapf;
+          };
+          auto-format = true;
+        }
+        {
+          name = "rust";
+          language-servers = [
+            "rust-analyzer"
+            "gpt"
+            "typos"
+            "wakatime"
+          ];
+        }
+        {
           name = "toml";
           language-servers = [
             "taplo"
-            "wakatime"
+            "gpt"
             "typos"
-          ];
-        }
-        {
-          name = "javascript";
-          language-servers = [
-
-            "typescript-language-server"
-
-          ];
-          auto-format = true;
-        }
-        {
-          name = "java";
-          language-servers = [ "jdtls" ];
-          auto-format = true;
-        }
-        {
-          name = "nix";
-          language-servers = [
-            "nixd"
             "wakatime"
+          ];
+        }
+        {
+          name = "typst";
+          language-servers = [
+            "tinymist"
+            "gpt"
             "typos"
-          ];
-          formatter.command = lib.getExe pkgs.pkgs.nixfmt-rfc-style;
-          auto-format = true;
-        }
-        {
-          name = "fish";
-          language-servers = [
-            "fish"
             "wakatime"
           ];
+          formatter.command = lib.getExe pkgs.typstyle;
+          auto-format = true;
         }
       ];
+
       language-server = {
+        bash-language-server = {
+          command = "${pkgs.nodePackages.bash-language-server}/bin/bash-language-server";
+          args = [ "start" ];
+        };
         fish = {
           command = lib.getExe pkgs.fish-lsp;
           args = [ "start" ];
           environment.fish_lsp_show_client_popups = "false";
         };
-
-        typescript-language-server = {
-          command = lib.getExe pkgs.nodePackages_latest.typescript-language-server;
+        gpt =
+          let
+            gpt-wrapped = pkgs.writeShellScriptBin "helix-gpt" ''
+              export COPILOT_API_KEY=$(cat ${config.sops.secrets.copilot-api-key.path})
+              ${lib.getExe pkgs.helix-gpt} "$@"
+            '';
+          in
+          {
+            command = lib.getExe gpt-wrapped;
+            args = [
+              "--handler"
+              "copilot"
+            ];
+          };
+        jdtls = {
+          command = "${pkgs.jdt-language-server}/bin/${
+            if lib.versionOlder pkgs.jdt-language-server.version "1.31.0" then
+              "jdt-language-server"
+            else
+              "jdtls"
+          }";
+          args = [ "--jvm-arg=-javaagent:${pkgs.lombok}/share/java/lombok.jar" ];
         };
-        gpt = {
-          command = lib.getExe pkgs.helix-gpt;
-          args = [
-            "--handler"
-            "ollama"
-            # "--logFile"
-            # "helix-gpt.log"
-          ];
+        ltex-ls = {
+          command = lib.getExe' pkgs.ltex-ls "ltex-ls";
+          config.ltex = {
+            disabledRules.en = [ "ARROWS" ];
+            disabledRules.fr = [ "FLECHES" ];
+            additionnalRules.enablePickyRules = true;
+            language = "auto";
+          };
         };
-        bash-language-server = {
-          command = "${pkgs.nodePackages.bash-language-server}/bin/bash-language-server";
-          args = [ "start" ];
-        };
-
         markdown-oxide.command = lib.getExe pkgs.markdown-oxide;
-
-        toml.command = lib.getExe pkgs.taplo;
-
-        nil = {
-          command = lib.getExe pkgs.nil;
-          config.nil.formatting.command = [ "${lib.getExe pkgs.nixfmt-rfc-style} --width=110" ];
-        };
-
         nixd = {
           command = lib.getExe pkgs.nixd;
           config = {
             nixpkgs.expr = "import <nixpkgs> { }";
-            options = {
-              nixos.expr = ''(builtins.getFlake "/home/louis/src/nixos-config").nixosConfigurations.magnus.options'';
-              # Only for standalone
-              # home-manager.expr = ''(builtins.getFlake "/home/louis/src/nixos-config").homeConfigurations."louis@magnus".options'';
-            };
+            options.nixos.expr = ''(builtins.getFlake "/home/louis/src/nixos-config").nixosConfigurations.magnus.options'';
           };
         };
-
-        tinymist = {
-          command = lib.getExe pkgs.tinymist;
-          config = {
-            exportPdf = "onType";
-          };
-        };
-
-        ocaml-lsp = {
-          command = lib.getExe pkgs.ocamlPackages.ocaml-lsp;
-        };
-        python-lsp = {
-          command = lib.getExe pkgs.python312Packages.python-lsp-server;
-        };
-
-        jdtls =
-          let
-            jdtls-bin =
-              if (lib.versionOlder pkgs.jdt-language-server.version "1.31.0") then
-                "jdt-language-server"
-              else
-                "jdtls";
-          in
-          {
-            command = "${pkgs.jdt-language-server}/bin/${jdtls-bin}";
-            args = [
-              "--jvm-arg=-javaagent:${pkgs.lombok}/share/java/lombok.jar"
-            ];
-          };
-
+        ocaml-lsp.command = lib.getExe pkgs.ocamlPackages.ocaml-lsp;
+        python-lsp.command = lib.getExe pkgs.python312Packages.python-lsp-server;
         rust-analyzer = {
           command = lib.getExe pkgs.rust-analyzer;
-          config = {
-            check = {
-              checkOnSave = true;
-              command = "clippy";
-              extraArgs = [
-                "--"
-                # "-W"
-                # "clippy::complexity"
-                "-W"
-                "clippy::perf"
-                # "-W"
-                # "clippy::style"
-                "-W"
-                "clippy::pedantic"
-                "-A"
-                "clippy::pedantic::missing_errors_doc"
-                # "-W"
-                # "clippy::nursery"
-              ];
-            };
+          config.check = {
+            checkOnSave = true;
+            command = "clippy";
+            extraArgs = [
+              "--"
+              "-W"
+              "clippy::perf"
+              "-W"
+              "clippy::pedantic"
+              "-A"
+              "clippy::pedantic::missing_errors_doc"
+            ];
           };
         };
-
-        ltex-ls = {
-          command = lib.getExe' pkgs.ltex-ls "ltex-ls";
-          config.ltex = {
-            disabledRules = {
-              en = [
-                "ARROWS"
-              ];
-              fr = [
-                "FLECHES"
-              ];
-            };
-            additionnalRules = {
-              enablePickyRules = true;
-            };
-            language = "auto";
-          };
+        taplo.command = lib.getExe pkgs.taplo;
+        tinymist = {
+          command = lib.getExe pkgs.tinymist;
+          config.exportPdf = "onType";
         };
-
-        wakatime.command = lib.getExe inputs.wakatime-lsp.packages.${pkgs.system}.default;
+        typescript-language-server.command = lib.getExe pkgs.nodePackages_latest.typescript-language-server;
         typos = {
           command = lib.getExe pkgs.typos-lsp;
           config.config = pkgs.writeText "typos.toml" ''
@@ -391,8 +328,8 @@
             ratatui = "ratatui"
           '';
         };
+        wakatime.command = lib.getExe inputs.wakatime-lsp.packages.${pkgs.system}.default;
       };
     };
-
   };
 }
