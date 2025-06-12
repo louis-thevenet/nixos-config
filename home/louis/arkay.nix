0d541d557ac7ea912a0923945e78d817e9c71a42
Louis Thevenet <louis.tvnt@gmail.com>
2025-06-12 15:20:40 +0200

    arkay: update monitors

--- home/louis/arkay.nix
+++ home/louis/arkay.nix
@@ -37,12 +37,22 @@
   };
   monitors = [
     {
+      name = "HDMI-A-1";
+      width = 1920;
+      height = 1080;
+      scale = 1.0;
+      x = 0;
+      y = 0;
+      workspace = "2";
+    }
+    {
       name = "eDP-1";
       width = 1920;
       height = 1080;
       refreshRate = 60.0;
       scale = 1.0;
       x = 0;
+      y = 1080;
       workspace = "1";
       primary = true;
     }
