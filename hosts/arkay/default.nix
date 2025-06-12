3a26c337ed6d9fef96621548d7098acd99b0ec4d
Louis Thevenet <louis.tvnt@gmail.com>
2025-06-12 15:58:10 +0200

    wip: init re6st and related services

--- hosts/arkay/default.nix
+++ hosts/arkay/default.nix
@@ -1,4 +1,12 @@
-{ ... }:
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  inherit (inputs.re6st.packages.${pkgs.system}) re6st;
+in
 {
   imports = [
     ./hardware-configuration.nix
@@ -17,5 +25,36 @@
     bluetooth.enable = true; # enables support for Bluetooth
     bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
     graphics.enable = true;
+  };
+  environment.defaultPackages = [
+    re6st
+  ];
+  environment.etc = {
+    "re6stnet/re6st-registry.conf" = {
+      text = ''
+
+      '';
+
+      mode = "0440";
+    };
+  };
+  systemd.user.services.re6stnet = {
+    description = "Resilient, Scalable, IPv6 Network application";
+    unitConfig.ConditionPathExists = "/etc/re6stnet/re6st-registry.conf";
+    StartLimitIntervalSec = 0;
+    script = ''
+      /bin/sh -c 'GEOIP2_MMDB=/etc/re6stnet/GeoLite2-Country.mmdb; [ -r $GEOIP2_MMDB ]
+      && export GEOIP2_MMDB; exec re6stnet @re6stnet.conf'
+    '';
+    wantedBy = "multi-user.target";
+  };
+  systemd.user.services.re6stnet-registry = {
+    description = "Server application for re6snet";
+    unitConfig.ConditionPathExists = "/etc/re6stnet/re6st-registry.conf";
+    StartLimitIntervalSec = 0;
+    script = ''
+      ${lib.getExe' re6st "re6st-registry"} @re6st-registry.conf
+    '';
+    wantedBy = "multi-user.target";
   };
 }
