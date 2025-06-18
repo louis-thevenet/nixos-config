249626ab0156e4b92ddf2144a94e9c2ae4e30bc4
Louis Thevenet <louis.thevenet@nexedi.com>
2025-06-18 09:48:10 +0200

    arkay: add swap file

--- hosts/arkay/hardware-configuration.nix
+++ hosts/arkay/hardware-configuration.nix
@@ -41,7 +41,13 @@
       ];
     };
   };
-  swapDevices = [ ];
+
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 16 * 1024;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
