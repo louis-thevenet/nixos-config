e6d884372479e5e60babe941de3ee35cf57fbbfe
Louis Thevenet <louis.tvnt@gmail.com>
2025-06-20 19:53:55 +0200

    fish: fix tmw and add rerun function

--- home/louis/global/tools/fish.nix
+++ home/louis/global/tools/fish.nix
@@ -15,8 +15,38 @@
       {
         tma.body = "${tmux} attach -t $(${tmux} list-sessions | ${tv} --preview 'tmux list-windows -t {0}' | ${cut} -d':' -f1)";
         tmw.body = ''
-          session=$(${tmux} list-sessions | ${tv} --preview 'tmux list-windows -t {0}' | ${cut} -d':' -f1);
-          ${tmux} attach -t ''${session}:$(${tmux} list-windows -t ''${session} | ${tv} | ${cut} -d':' -f1)
+          set session (${tmux} list-sessions | ${tv} --preview 'tmux list-windows -t {0}' | ${cut} -d':' -f1)
+          ${tmux} attach -t $session:(${tmux} list-windows -t $session | ${tv} | ${cut} -d':' -f1)
+        '';
+        rerun = ''
+          argparse 'd/dry-run' -- $argv
+          if test (count $argv) -ne 1
+              echo "Usage: rerun [--dry-run|-d] N"
+              return 1
+          end
+
+          set -l n $argv[1]
+          set -l count 0
+          set -l commands
+
+          for cmd in (history | grep -v '^rerun') # skip rerun calls
+              if test $count -ge $n
+                  break
+              end
+              set commands $cmd $commands  # prepend to preserve order
+              set count (math $count + 1)
+          end
+
+          if test $count -lt $n
+              echo "Only found $count non-rerun commands in history"
+          end
+
+          for cmd in $commands
+              echo "> $cmd"
+              if not set -q _flag_dry_run
+                  eval $cmd
+              end
+          end
         '';
       };
 
@@ -30,7 +60,6 @@
     };
 
     shellAliases =
-
       let
         eza = lib.getExe pkgs.eza;
         helix = lib.getExe pkgs.helix;
@@ -41,7 +70,6 @@
         nh = lib.getExe pkgs.nh;
         git = lib.getExe pkgs.git;
       in
-
       {
         which = "readlink -f (type -p $argv)";
         rebuild-dagon = "cd /home/louis/src/nixos-config && nixos-rebuild switch --flake .#dagon --target-host louis@dagon --use-remote-sudo --impure";
