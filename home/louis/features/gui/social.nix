a2a3a8841a8dc15c526e8f43f29a2fd7c4488193
Louis Thevenet <louis.tvnt@gmail.com>
2025-06-29 13:50:19 +0200

    beeper: remove wayland fix

--- home/louis/features/gui/social.nix
+++ home/louis/features/gui/social.nix
@@ -7,24 +7,12 @@
 let
   inherit (lib) mkIf;
   cfg-social = config.home-config.gui;
-  cfg-wayland = config.home-config.desktop.wayland;
-
-  beeper-wayland = pkgs.symlinkJoin {
-    name = "beeper";
-    paths = [
-      pkgs.beeper
-    ];
-    buildInputs = [ pkgs.makeWrapper ];
-    postBuild = ''
-      wrapProgram $out/bin/beeper --add-flags "--ozone-platform=wayland"
-    '';
-  };
 in
 {
   home.packages = mkIf cfg-social.social.enable (
     with pkgs;
     [
-      (if cfg-wayland.enable then beeper-wayland else pkgs.beeper)
+      beeper
       discord
     ]
   );
