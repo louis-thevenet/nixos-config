fc07a1e85cfb3b3eed0ffe1582fc505f0c01149b
Louis Thevenet <louis.tvnt@gmail.com>
2025-07-15 09:18:10 +0200

    nix: disable GC

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -23,7 +23,6 @@
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
-
         "https://nix-ai-stuff.cachix.org"
         "https://cuda-maintainers.cachix.org"
         "https://ai.cachix.org"
@@ -31,7 +30,6 @@
 
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-
         "nix-ai-stuff.cachix.org-1:WlUGeVCs26w9xF0/rjyg32PujDqbVMlSHufpj1fqix8="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
@@ -41,10 +39,7 @@
 
   programs.nh = {
     enable = true;
-    clean = {
-      enable = true;
-      extraArgs = "--keep 3 --keep-since 7d";
-    };
+    clean.enable = false;
     flake = "/home/louis/src/nixos-config";
   };
 }
