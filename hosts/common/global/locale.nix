41716bf266cf26e42573c125219791ec240f8f03
Louis Thevenet <louis.tvnt@gmail.com>
2025-08-30 07:57:12 -0400

    locale: change to montreal

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -1,6 +1,5 @@
-_:
-{
-  time.timeZone = "Europe/Paris";
+_: {
+  time.timeZone = "America/Toronto";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "fr_FR.UTF-8";
--- hosts/dagon/glance.nix
+++ hosts/dagon/glance.nix
@@ -89,7 +89,7 @@ in
                     hour-format = "24h";
                     hide-location = false;
                     show-area-name = true;
-                    location = "Lille, France";
+                    location = "Montreal, Canada";
                   }
                   {
                     type = "bookmarks"; # https://github.com/glanceapp/glance/blob/main/docs/configuration.md#bookmarks
