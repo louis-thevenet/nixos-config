7e74803849c2d7a21d37b8084af196d572f6d249
Louis Thevenet <louis.tvnt@gmail.com>
2025-09-29 13:16:25 -0400

    virt: replace podman with docker

--- hosts/common/optional/virt.nix
+++ hosts/common/optional/virt.nix
@@ -1,14 +1,15 @@
 _: {
   virtualisation = {
     libvirtd.enable = true;
-    podman = {
-      enable = true;
-      dockerCompat = true;
-    };
+    # podman = {
+    #   enable = true;
+    #   dockerCompat = true;
+    # };
     virtualbox.host = {
       enable = true;
       enableExtensionPack = true;
     };
+    docker.enable = true;
   };
   users.users.louis.extraGroups = [ "libvirtd" ];
 }
