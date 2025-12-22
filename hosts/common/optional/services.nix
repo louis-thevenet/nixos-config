d7fdb1848a214c142c11d635fc2ccceb986ae32f
Louis Thevenet <louis.tvnt@gmail.com>
2025-12-22 11:43:54 +0100

    all: make bluetooth headset mic work

--- hosts/common/optional/services.nix
+++ hosts/common/optional/services.nix
@@ -8,7 +8,17 @@ _: {
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+
+      wireplumber.enable = true;
+      extraConfig.pipewire."bluez-monitor" = {
+        "properties" = {
+          "bluez5.hfphfp-backend" = "native";
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-sbc-xq" = true;
+        };
+      };
     };
+
     logind.extraConfig = ''
       # don’t shutdown when power button is short-pressed
       HandlePowerKey=ignore
