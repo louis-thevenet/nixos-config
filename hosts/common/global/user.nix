3d0f1cc04e2c0b864523589a6bb71cdc086de344
Louis Thevenet <louis.tvnt@gmail.com>
2026-07-15 18:42:18 +0200

    wip

--- hosts/common/global/user.nix
+++ hosts/common/global/user.nix
@@ -36,9 +36,9 @@ in
     };
   };
   # TODO: MOVE THIS
-  sops.secrets.wakatime_cfg = {
-    sopsFile = ../secrets.yaml;
-    path = "/home/louis/.wakatime.cfg";
-    owner = "louis";
-  };
+  # sops.secrets.wakatime_cfg = {
+  #   sopsFile = ../secrets.yaml;
+  #   path = "/home/louis/.wakatime.cfg";
+  #   owner = "louis";
+  # };
 }
