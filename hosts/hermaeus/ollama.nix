a81ac3ae754de6f7812dd7d03029b2d5e4447446
Louis Thevenet <louis.tvnt@gmail.com>
2026-07-18 12:54:46 +0200

    hosts/hermaeus: open ollama to lan

--- hosts/hermaeus/ollama.nix
+++ hosts/hermaeus/ollama.nix
@@ -1,4 +1,13 @@
-{ pkgs, ... }: {
+{ ... }:
+let
+  host = "192.168.1.183";
+  port = 11434;
+in
+{
+  networking.firewall.allowedTCPPorts = [ port ];
+
+  environment.variables.OLLAMA_HOST = host; # so i can run stuff like `ollama pull`
+
   services.ollama = {
     enable = true;
     # package = pkgs.ollama-rocm;
@@ -6,6 +15,6 @@
       OLLAMA_KEEP_ALIVE = "10m";
       OLLAMA_CONTEXT_LENGTH = "200000";
     };
+    inherit host port;
   };
-  environment.systemPackages = [ pkgs.llama-cpp ];
 }
